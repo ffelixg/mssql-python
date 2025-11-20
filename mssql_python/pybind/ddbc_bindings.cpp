da23ac644549a44e28243d9148d4d1d233e589dd
ffelixg <142172984+ffelixg@users.noreply.github.com>
2025-11-20 21:24:10 +0100

    Adapt LOG change

--- mssql_python/pybind/ddbc_bindings.cpp
+++ mssql_python/pybind/ddbc_bindings.cpp
@@ -4180,7 +4180,7 @@ SQLRETURN FetchArrowBatch_wrap(SqlHandlePtr StatementHandle, py::list& capsules)
                 std::ostringstream errorString;
                 errorString << "Unsupported data type for Arrow batch fetch for column - " << columnName.c_str()
                             << ", Type - " << dataType << ", column ID - " << (i + 1);
-                LOG(errorString.str());
+                LOG(errorString.str().c_str());
                 ThrowStdException(errorString.str());
                 break;
         }
@@ -4446,7 +4446,7 @@ SQLRETURN FetchArrowBatch_wrap(SqlHandlePtr StatementHandle, py::list& capsules)
                     std::ostringstream errorString;
                     errorString << "Unsupported data type for column - " << columnName.c_str()
                                 << ", Type - " << dataType << ", column ID - " << col;
-                    LOG(errorString.str());
+                    LOG(errorString.str().c_str());
                     ThrowStdException(errorString.str());
                     break;
                 }
@@ -4543,7 +4543,7 @@ SQLRETURN FetchArrowBatch_wrap(SqlHandlePtr StatementHandle, py::list& capsules)
                 std::ostringstream errorString;
                 errorString << "Unsupported data type for column - " << columnName.c_str()
                             << ", Type - " << dataType << ", column ID - " << (col + 1);
-                LOG(errorString.str());
+                LOG(errorString.str().c_str());
                 ThrowStdException(errorString.str());
                 break;
             }
