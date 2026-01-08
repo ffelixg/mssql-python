cbb333167a90f36042ff081dcfed5272abcf7212
ffelixg <142172984+ffelixg@users.noreply.github.com>
2026-01-08 18:57:51 +0100

    bye

--- tests/test_004_cursor.py
+++ tests/test_004_cursor.py
@@ -15110,5 +15110,3 @@ def test_varchar_latin1_fetch(cursor):
         query()
         with pytest.raises(SystemError, match=".*returned a result with an exception set"):
             cursor.fetchmany(500)
-        1/0
-    
